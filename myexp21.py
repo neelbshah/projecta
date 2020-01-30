e9fcee173938b14d2b9c78f21fd9fb245f7c1f82
neelbshah <51773521+neelbshah@users.noreply.github.com>
2020-01-30 10:00:48 +0530

    Add files via upload

---
+++ myexp21.py
@@ -0,0 +1,11 @@
+s1="HELLO WORLD"
+s2="THIS IS PYTHON PROGRAMMING"
+print s1
+print s2
+print s1.title()
+print s1.isdigit()
+print s1.islower()
+print max(s2)
+print min(s1)
+print s2.split()
+print s2.split(" ",2)
